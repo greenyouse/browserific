45692b2829b3e322bd8deb1ac46f3aa184a9a48e
greenyouse <greenyouse@hotmail.com>
2014-11-09 21:57:39 -0600

    Merged config GUI with rest of the framework

---
+++ lein-browserific/src/clj/browserific/helpers/utils.clj
@@ -0,0 +1,29 @@
+(ns browserific.helpers.utils
+  (:require [leiningen.core.main :as l]))
+
+(def ^:private options
+  "Map of browserific options"
+  (let [p (-> "project.clj" slurp read-string (nthrest 3))
+        psplit (partition 2 p)]
+    (reduce #(into %1 [(into [] %2)])
+            {} psplit)))
+
+(def config-file
+  "Path of the selected config.edn file to use"
+  (or (:config (:browserific options)) "src/config.edn"))
+
+(defn get-config
+  "Helper fn for getting data from the config-file"
+  [coll]
+  (let [conf (-> config-file slurp read-string)]
+    (get-in conf coll)))
+
+(defn red-text [msg] (str "\033[31m" msg "\033[0m"))
+
+(defn config-warning [e]
+  (l/warn (str (red-text e) "\n\n")))
+
+(def systems
+  {:browsers (try (get-config [:extensions :platforms]) (catch Exception e (config-warning e)))
+   :mobile (try (get-config [:mobile :platforms]) (catch Exception e (config-warning e)))
+   :desktop (try (get-config [:desktop :platforms]) (catch Exception e (config-warning e)))})
