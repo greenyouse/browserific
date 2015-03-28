7a44f1c5ed36040f4d75bcd8578c326cd8ef9542
greenyouse <greenyouse@hotmail.com>
2015-03-28 02:18:36 -0500

    Update to latest chenex

--- browserific-app/src/leiningen/new/browserific_app/project.clj
+++ browserific-app/src/leiningen/new/browserific_app/project.clj
@@ -4,23 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+                 [org.clojure/clojurescript "0.0-3119"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.greenyouse/pldb-cache "0.1.0-webstorage"]
-                 [com.greenyouse/chenex "0.1.2"]
-                 [reagent "0.5.0"]
-                 [cljsjs/react "0.12.2-5"]]
+                 [com.greenyouse/chenex "0.1.4"]
+                 [reagent "0.5.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [com.greenyouse/chenex "0.1.2"]
-            [com.cemerick/clojurescript.test "0.3.1"]
-            [lein-browserific "0.1.1-alpha"]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [com.greenyouse/chenex "0.1.4"]
+            [com.cemerick/clojurescript.test "0.3.3"]
+            [lein-browserific "0.1.2-alpha"]
             [lein-auto "0.1.1"]
-            [lein-figwheel "0.2.2-SNAPSHOT"]]
+            [lein-figwheel "0.2.5"]]
 
   :profiles {:default [:base :system :user :provided :dev :plugin.chenex/default]
              :dev {:dependencies [[weasel "0.6.0"]
-                                  [figwheel "0.2.2-SNAPSHOT"]]}}
+                                  [figwheel "0.2.5"]]}}
 
   :browserific {}
 
--- lein-browserific/project.clj
+++ lein-browserific/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-browserific "0.1.1-alpha"
+(defproject lein-browserific "0.1.2-alpha"
   :description "A Leiningen build tool for unified app development"
   :url "https://github.com/greenyouse/browserific/tree/master/lein-browserific"
   :license {:name "Eclipse Public License"
@@ -12,7 +12,7 @@
                  [fogus/ring-edn "0.2.0"]
                  [compojure "1.2.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.greenyouse/chenex "0.1.2"]
+                 [com.greenyouse/chenex "0.1.4"]
                  ;; cljs
                  [org.clojure/clojurescript "0.0-3119"]
                  [reagent "0.5.0"]]
